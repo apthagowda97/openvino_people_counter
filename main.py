339f871d4ffe4263a88630edebfc5bd0c2a59b71
apthagowda97 <iamuraptha@gmail.com>
2020-06-12 13:39:07 +0530

    Updated the single image mode and added comments explaining the logic

--- main.py
+++ main.py
@@ -40,11 +40,11 @@ MQTT_HOST = IPADDRESS
 MQTT_PORT = 3001
 MQTT_KEEPALIVE_INTERVAL = 60
 
-infr_arr = []
-duration_arr = []
+infr_arr = [] #to store the inference time
+duration_arr = [] #to store the duration
 
-start_time = 0
-temp_flag = 0
+single_image_mode = False
+start_time = 0 
 count = 0
 total = 0
 duration = 0.0
@@ -91,29 +91,27 @@ def draw_boxes(frame, result,prob_threshold, width, height):
     for i,box in enumerate(result[0][0]):
         conf = box[2]
         if conf >= prob_threshold:
-            new_conf = conf
+            new_conf = conf 
             xmin = int(box[3] * width)
             ymin = int(box[4] * height)
             xmax = int(box[5] * width)
             ymax = int(box[6] * height)
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255,0,0), 2)
-        
+    #new_conf will send 0 if confiedence is below prob_threshold else it will send conf
     return frame,new_conf
 
 def postprocess(conf,frame_no,conf_arr,client,fps):
-    global start_time,temp_flag,count,total,duration
-    if temp_flag == 0:
+    global start_time,count,total,duration
+    if count == 0: 
         if conf > 0:
-            temp_flag = 1
-            count = 1
-            total = total + 1
-            start_time = frame_no
-    if temp_flag == 1:
+            count = 1 # if conf is greater than 0 then count will be updated as 1
+            total = total + 1 # the total count of the people is increamented by 1
+            start_time = frame_no #current frame number is stored in start_frame
+    if count == 1:
         post_frames = conf_arr[-3:]
-        if np.mean(post_frames) == 0:
-            temp_flag = 0
+        if np.mean(post_frames) == 0: #if consecutive 3 frame gives 0 then count will be updated as 0
             count = 0
-            duration = ((frame_no)-start_time)/fps
+            duration = ((frame_no)-start_time)/fps #duration is calculated by subtracting current frame with start_frame divide by fps.
             client.publish("person/duration", json.dumps({"duration": duration}))
             duration_arr.append(duration)
     client.publish("person", json.dumps({"count": count, "total":total}))
@@ -128,8 +126,11 @@ def infer_on_stream(args,client):
     :param client: MQTT client
     :return: None
     """
+    
+    global single_image_mode
     # Initialise the class
     infer_network = Network()
+    
     # Set Probability threshold for detections
     prob_threshold = args.prob_threshold
 
@@ -137,22 +138,31 @@ def infer_on_stream(args,client):
     plugin = Network()
     plugin.load_model(args.model, args.device, args.cpu_extension)
     net_input_shape = plugin.get_input_shape()
+    
     ### TODO: Handle the input stream ###
+    if args.input == 'CAM':
+        args.input = 0
+        
+    elif args.input.endswith('.jpg') or args.input.endswith('.bmp'):
+        single_image_mode = True 
+        
     cap = cv2.VideoCapture(args.input)
     cap.open(args.input) 
+    
     width = int(cap.get(3))
     height = int(cap.get(4))
-    fps = cap.get(cv2.CAP_PROP_FPS)
+    fps = cap.get(cv2.CAP_PROP_FPS) # storing the fps of the video
+    
     ### TODO: Loop until stream is over ###
     frame_no = 0
     conf_arr = []
-    
     while cap.isOpened():
         ### TODO: Read from the video capture ###
         flag, frame = cap.read()
         frame_no = frame_no+1
         start_infr = time.time()
-        if frame_no%(args.frame_skip_rate+1)== 0:
+        if frame_no%(args.frame_skip_rate+1)== 0 or single_image_mode==True:
+            # frame will be skipped based on the -s argument (frame_skip_rate) to decrease the inference time.
             if not flag:
                 break
             ### TODO: Pre-process the image as needed ###
@@ -161,8 +171,10 @@ def infer_on_stream(args,client):
             p_frame = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]))
             p_frame = p_frame.transpose((2,0,1))
             p_frame = p_frame.reshape(1, *p_frame.shape)
+
             ### TODO: Start asynchronous inference for specified request ###
             plugin.exec_net(p_frame)
+
             ### TODO: Wait for the result ###
             if plugin.wait() == 0:
                 ### TODO: Get the results of the inference request ###
@@ -170,23 +182,30 @@ def infer_on_stream(args,client):
                 ### TODO: Extract any desired stats from the results ###
                 out_frame,conf = draw_boxes(new_frame,result,prob_threshold,width,height)
                 conf_arr.append(conf)
+
                 ### TODO: Calculate and send relevant information on ###
                 ### current_count, total_count and duration to the MQTT server ###
                 ### Topic "person": keys of "count" and "total" ###
                 ### Topic "person/duration": key of "duration" ###
                 postprocess(conf,frame_no,conf_arr,client,fps)
                 infr_arr.append((time.time()-start_infr))
+
             ### TODO: Send the frame to the FFMPEG server ###
             sys.stdout.buffer.write(out_frame)
             sys.stdout.flush()
             if key_pressed == 27:
                 break
             ### TODO: Write an output image if `single_image_mode` ###
+            if single_image_mode:
+                cv2.imwrite('output_image.jpg', out_frame)
     cap.release()
     cv2.destroyAllWindows()
     client.disconnect()
     
 def end_result(args):
+    #It is used to store the result in result.txt to do better documentation.
+    if single_image_mode==True:
+        return
     file = open("result.txt", "a")
     data = "Model: {0}\n\
     prob_threshold: {1}\n\
@@ -211,6 +230,7 @@ def main():
     client = connect_mqtt()
     # Perform inference on the input stream
     infer_on_stream(args,client)
+    #upate the result.txt with result
     end_result(args)
 
 
